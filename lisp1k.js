f773aa1b19204fc8c56c445602c93dd2ba4244c9
Tony Onodi <tonyonodi@gmail.com>
2014-10-05 12:39:51 +0100

    Remove empty tokens from token array.

--- lisp1k.js
+++ lisp1k.js
@@ -1,19 +1,47 @@
-E=function(i){
+var evaluate = function(input){
     var lexed, parsed;
 
-    lexed = i.replace(/\(/g, " ( ").replace(/\)/g, " ) ").split(" ");
-
-    return lexed
+    lexed = input.replace(/\(/g, " ( ").replace(/\)/g, " ) ").split(" ");
+    
+    console.log(input);
+    console.log(lexed);
+    console.log(parse(lexed));
 }
 
-O = function(i) {
+var parse = function(tokens){
+    
+    // remove all empty strings from array
+    var index = tokens.indexOf("");
+    while(index != -1) {
+        tokens.splice(index, 1);
+        index = tokens.indexOf("");
+    }
+    console.log(tokens);
+
+    // remove first token and save
+    var token = tokens.shift();
+
+    if (token == "(") {
+        var list = [];
+        console.log("loop happening")
+        while (token != ")") {
+            list.push(parse(token));
+        }
+        tokens.shift();  // pop off ")"
+        return list;
+    } else {
+        return token;   // return atom
+    }
+}
+
+var output = function(i) {
     document.writeln("<br>"+i)
 }
 
 while(1) {
     I = window.prompt(">>>");
-    O(">"+I);   // output command
-    O(E(I)) // output evaluated command
+    output(">"+I);   // output command
+    output(evaluate(I)) // output evaluated command
 }
 
 
